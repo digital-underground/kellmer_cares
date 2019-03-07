0622b79aacb2def6ef2c1e4ef038a9903b9d74f7
Robert Brown <robertlb4@gmail.com>
2019-03-06 18:36:03 -0800

    refactor to update audio src, to work on ios

--- js/index.js
+++ js/index.js
@@ -1,16 +1,23 @@
 'use strict';
 
-
+let beep = document.getElementById('myBeep');
 let events = [1, 5, 10, 15, 30, 60, 90, 120];
 let x;
 
-function playAudio(time) {
-    let beep = document.getElementById('myBeep');
-    let audio = document.getElementById(`${time}minute`);
-    beep.play();
-    audio.play();
-    events = events.slice(1);
-}
+beep.addEventListener('ended', () => {
+    console.log('i fired');
+    beep.src = `data/${events[0]}_minutes.m4a`;
+    beep.load();
+});
+
+
+// function playAudio(time) {
+
+//     let audio = document.getElementById(`${time}minute`);
+//     beep.play();
+//     audio.play();
+
+// }
 
 function calcTime(milliseconds) {
     return {
@@ -34,48 +41,49 @@ function reset() {
     toggleStartButton(false);
 }
 
-function addAudio() {
-    let audioContainer = document.getElementById('audio-container');
-    audioContainer.innerHTML = `
-    <audio id="1minute" source src="data/1_minute.mp3" type="audio/mp3" autostart="false">
-    </audio>
+// function addAudio() {
+    // let audioContainer = document.getElementById('audio-container');
+    // audioContainer.innerHTML = `
+    // <audio id="1minute" source src="data/1_minute.mp3" type="audio/mp3" autostart="false">
+    // </audio>
 
-    <audio id="5minute">
-        <source src="data/5_minutes.m4a" type="audio/mp4">
-    </audio>
+    // <audio id="5minute">
+    //     <source src="data/5_minutes.m4a" type="audio/mp4">
+    // </audio>
 
-    <audio id="10minute">
-        <source src="data/10_minutes.m4a" type="audio/mp4">
-    </audio>
+    // <audio id="10minute">
+    //     <source src="data/10_minutes.m4a" type="audio/mp4">
+    // </audio>
 
-    <audio id="15minute">
-        <source src="data/15_minutes.m4a" type="audio/mp4">
-    </audio>
+    // <audio id="15minute">
+    //     <source src="data/15_minutes.m4a" type="audio/mp4">
+    // </audio>
 
-    <audio id="30minute">
-        <source src="data/30_minutes.m4a" type="audio/mp4">
-    </audio>
+    // <audio id="30minute">
+    //     <source src="data/30_minutes.m4a" type="audio/mp4">
+    // </audio>
 
-    <audio id="60minute">
-        <source src="data/60_minutes.m4a" type="audio/mp4">
-    </audio>
+    // <audio id="60minute">
+    //     <source src="data/60_minutes.m4a" type="audio/mp4">
+    // </audio>
 
-    <audio id="90minute">
-        <source src="data/90_minutes.m4a" type="audio/mp4">
-    </audio>
+    // <audio id="90minute">
+    //     <source src="data/90_minutes.m4a" type="audio/mp4">
+    // </audio>
 
-    <audio id="120minute">
-        <source src="data/120_minutes.m4a" type="audio/mp4">
-    </audio>
+    // <audio id="120minute">
+    //     <source src="data/120_minutes.m4a" type="audio/mp4">
+    // </audio>
 
-    <audio id="myBeep">
-        <source src="data/beep.mp3" type="audio/mp4">
-    `;
-}
+    // <audio id="myBeep">
+    //     <source src="data/beep.mp3" type="audio/mp4">
+    // `;
+// }
 
 function myFunction() {
     toggleStartButton(true);
-    addAudio();
+    // addAudio();
+    beep.play();
     let countDownDate = new Date();
 
     // Update the count down every 1 second
@@ -93,7 +101,10 @@ function myFunction() {
 
         //play appropriate recording based on elapsed time.
         if (Math.floor(distance / 10000) === events[0]) {
-            playAudio(events[0]);
+            // playAudio(events[0]);
+            console.log(distance / 10000);
+            beep.play();
+            events = events.slice(1);
         }
 
     }, 333);
