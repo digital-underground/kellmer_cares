c9a5303dee63238b65d7c54f8059c1b651038660
Robert Brown <robertlb4@gmail.com>
2019-03-11 12:45:57 -0700

    change to iterating form slicing to allow full reset back to beginning. reset to remove listener

--- js/index.js
+++ js/index.js
@@ -3,12 +3,13 @@
 let audio = document.getElementById('myBeep');
 let events = [1, 5, 10, 15, 30, 60, 90, 120];
 let x;
+let i = 0
 
 //update audio src with the next audio file
-audio.addEventListener('ended', () => {
-    audio.src = `data/${events[0]}_minutes.m4a`;
+function setAudioSrc() {
+    audio.src = `data/${events[i]}_minutes.m4a`;
     audio.load();
-});
+}
 
 function calcTime(milliseconds) {
     return {
@@ -29,32 +30,33 @@ function toggleStartButton(status) {
 function reset() {
     clearInterval(x);
     updateClock(calcTime(0));
+    i = 0;
+    audio.pause()
+    audio.src = `data/beep.mp3`
+    audio.removeEventListener('ended', setAudioSrc)
     toggleStartButton(false);
 }
 
 function main() {
     toggleStartButton(true);
+    //update audio src with the next audio file
+    audio.addEventListener('ended', setAudioSrc);
     audio.play();
     let countDownDate = new Date();
-
     // Update the count down every 1 second
     x = setInterval(function () {
-
         // Get todays date and time
         let now = new Date().getTime();
-
         // Find the distance between now an the count down date
         let distance = now - countDownDate.getTime();
-
         // Time calculations for days, hours, minutes and seconds and
         // Output the result in an element with id="demo"
         updateClock(calcTime(distance));
-
         //play appropriate recording based on elapsed time.
-        if (Math.floor(distance / 60000) === events[0]) {
+        if (Math.floor(distance / 60000) === events[i]) {
             audio.play();
             //move to next audio event
-            events = events.slice(1);
+            i++;
         }
     }, 333);
 }
